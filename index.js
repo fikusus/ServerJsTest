a970bbfa0f972ad9d88d94971ae2aee89b486fb0
danilakuzmuk <danila.kuzmuk@gmail.com>
2021-02-05 13:54:15 +0200

    IP

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { promises } = require('fs');
 
+let old = new Date();
+
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 io.on('connection', client => {
     console.log("connection");
-  client.on('event', data => { console.log("data")});
+  client.on('event', data => { 
+    console.log(new Date() - old)
+    old = new Date();
+  });
   client.on('disconnect', () => { /* … */ });
 });
 server.listen(process.env.PORT || 3000);
